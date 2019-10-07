778c47990b240b94ceb29c7cbfa92265ea6a8fbe
Diego Tabares <tabares@ar.ibm.com>
2019-10-07 10:45:24 -0300

    changing Jenkins build params since the instance died some times

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -14,7 +14,7 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 
 # Set up Jenkins with sufficient resources
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=4Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
-oc set resources dc jenkins --limits=memory=4Gi,cpu=2 --requests=memory=2Gi,cpu=500m -n ${GUID}-jenkins
+oc set resources dc jenkins --limits=memory=4Gi,cpu=4 --requests=memory=3Gi,cpu=2 -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
 oc new-build --strategy=docker -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
